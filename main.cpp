d9b67a03c11a1eaf259e03ea4109fd45a36b1c3c
Elina Iskhakova <35336512+ElinaIskhakova@users.noreply.github.com>
2018-01-16 17:00:21 +0500

    Calc time for selection sort and radix sort

--- main.cpp
+++ main.cpp
@@ -8,41 +8,76 @@
 using namespace std;
 void Selection(int*, int);
 void radix(int *,int*,int*,int,int);
-
 int main()
 {
     srand(time(NULL));
     setlocale(LC_ALL, "rus");
-    cout << "Введите размер массива: ";
-    int size_array; // длина массива
+    cout << "Length of array: ";
+    int size_array;
+    int*c= new int [size_array];
+    int*br= new int [size_array];
     cin >> size_array;
-
-    int *sorted_array = new int [size_array]; // одномерный динамический массив
+    int *sorted_array = new int [size_array];
+    int *sorted_array1 = new int [size_array];
     for (int counter = 0; counter < size_array; counter++)
     {
-        sorted_array[counter] = rand() % 100; // заполняем массив случайными числами
-        cout << setw(2) << sorted_array[counter] << "  "; // вывод массива на экран
+        sorted_array[counter] = rand() % 100;
+        cout << setw(2) << sorted_array[counter] << "  ";
+    }
+    for (int counter = 0; counter < size_array; counter++)
+    {
+        sorted_array1[counter] = rand() % 100;
+        cout << setw(2) << sorted_array1[counter] << "  ";
     }
     cout << "\n\n";
     clock_t start=clock();
-    Selection(sorted_array, size_array);
+    radix(sorted_array,br,c,size_array, size_array);
     clock_t end=clock();
 double seconds=(double)(end-start)/CLOCKS_PER_SEC;
+clock_t start1=clock();
+Selection(sorted_array1, size_array);
+clock_t end1=clock();
+double seconds1=(double)(end1-start1)/CLOCKS_PER_SEC;
+cout<<"Array of radix:\n";
     for (int counter = 0; counter < size_array; counter++)
     {
-        cout << setw(2) << sorted_array[counter] << "  ";
+        cout << setw(2) << br[counter] << "  ";
     }
     cout << "\n";
-    printf("The time: %f seconds\n", seconds);
-    delete [] sorted_array; // высвобождаем память
+    cout<<"Array of selection\n";
+    for (int counter = 0; counter < size_array; counter++)
+    {
+        cout << setw(2) << sorted_array1[counter] << "  ";
+    }
+    printf("The time: %f seconds of Radix\n", seconds);
+    printf("The time: %f seconds of Selection\n", seconds1);
+    delete [] sorted_array;
+    delete [] sorted_array1;
     system("pause");
     return 0;
 }
-void Selection(int* arrayPtr, int length_array) // сортировка выбором
+void radix(int *ar, int *br, int *cr, int sizeC, int sizeAB)
+{
+    int i,k,n;
+    k = sizeC;
+    n = sizeAB;
+    for (i = 0; i <= k; i++)
+        cr[i]=0;
+    for (i = 0; i < n; i++)
+        cr[ar[i]]+=1;
+    for (i = 1; i < k+1; i++)
+        cr[i]+=cr[i-1];
+    for (i = n-1; i >= 0; i--)
+      {
+         br[cr[ar[i]]-1]=ar[i];
+         cr[ar[i]]-=1;
+      }
+  }
+void Selection(int* arrayPtr, int length_array) // selection
 {
     for (int repeat_counter = 0; repeat_counter < length_array; repeat_counter++)
     {
-        int temp = arrayPtr[0]; // временная переменная для хранения значения перестановки
+        int temp = arrayPtr[0];
         for (int element_counter = repeat_counter + 1; element_counter < length_array; element_counter++)
         {
             if (arrayPtr[repeat_counter] > arrayPtr[element_counter])
